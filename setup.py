20fb8d45ced6ac36d5d2955b65c1664beebb8e80
Ariel Nunez <ingenieroariel@gmail.com>
2013-11-21 17:58:43 -0500

    Initial work on getting an installable package

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+from distutils.core import setup
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(
+    name="geogit",
+    version="0.1",
+    requires=['Python (>=2.6)', 'Shapely (>=1.3.0)'],
+    author="",
+    author_email="",
+    description="Python bindings for GeoGit",
+    long_description=(read('README')),
+    # Full list of classifiers can be found at:
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 1 - Planning',
+    ],
+    license="BSD",
+    keywords="geogit",
+    url='https://github.com/volaya/geogitpy',
+    packages=['src/geogit/',],
+    include_package_data=True,
+    zip_safe=False,
+)
