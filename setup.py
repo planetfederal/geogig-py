2e0a938180f1af93c438b0356b6d79f6587a9ecb
Victor Olaya <volayaf@gmail.com>
2013-12-10 18:26:51 +0100

    changed setup to use easy_install

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from distutils.core import setup
+from setuptools import setup
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
@@ -7,7 +7,7 @@ def read(*rnames):
 setup(
     name="geogit-py",
     version="0.1",
-    requires=['Python (>=2.6)', 'Shapely (>=1.3.0)', 'py4j (>=0.8)'],
+    install_requires=['Shapely>=1.2.9', 'py4j>=0.8'],
     author="Victor Olaya",
     author_email="volaya@boundlessgeo.com",
     description="Python bindings for GeoGit",
@@ -21,7 +21,5 @@ setup(
     keywords="geogit",
     url='https://github.com/boundlessgeo/geogit-py',
     package_dir={'': 'src'},
-    packages=['geogitpy',],
-    include_package_data=True,
-    zip_safe=False,
+    packages=['geogitpy',]
 )
