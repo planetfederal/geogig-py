27827a7400d604440326503d034759bb53304298
Victor Olaya <volayaf@gmail.com>
2013-12-10 18:28:19 +0100

    added imports to package init

---
+++ src/geogitpy/__init__.py
@@ -0,0 +1,7 @@
+from geogit import *
+from repo import Repository
+from commitish import Commitish
+from feature import Feature
+from tree import Tree
+from diff import Diffentry
+from commit import Commit
