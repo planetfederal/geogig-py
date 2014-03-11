8d74dd4371a78b6daee09e41951b6bac1e1925b5
Victor Olaya <volayaf@gmail.com>
2014-03-11 10:35:27 +0100

    changed version to 0.7

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(*rnames):
 
 setup(
     name="geogit-py",
-    version="0.1.2",
+    version="0.7",
     install_requires=['Shapely>=1.2.9', 'py4j>=0.8', 'geojson>=1.0.5'],
     author="Victor Olaya",
     author_email="volaya@boundlessgeo.com",
