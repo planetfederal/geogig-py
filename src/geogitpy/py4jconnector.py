fa6eca6822c6a0a37142da3a3305efbac6c3497c
Victor Olaya <volayaf@gmail.com>
2014-03-11 10:36:13 +0100

    allow py4j connector to use a custom port

--- src/geogitpy/py4jconnector.py
+++ src/geogitpy/py4jconnector.py
@@ -1,4 +1,4 @@
-from py4j.java_gateway import JavaGateway
+from py4j.java_gateway import JavaGateway, GatewayClient
 import logging
 from geogitexception import GeoGitException
 from cliconnector import CLIConnector
@@ -11,17 +11,27 @@ import signal
 _proc = None
 _gateway = None
 _geogitPath = None
+_geogitPort = None
 
 _logger = logging.getLogger("geogitpy")
 
 def setGeoGitPath(path):
     global _geogitPath
     _geogitPath = path
+    
+def setGatewayPort(port):
+    global _geogitPort, __gateway
+    if _geogitPort != port:
+        _geogitPort = port
+        shutdownServer()
 
 def _connect():    
     global _gateway
     try: 
-        _gateway = JavaGateway()       
+        if _geogitPort is None:
+            _gateway = JavaGateway()
+        else:
+            _gateway = JavaGateway(GatewayClient(port = _geogitPort))       
         _gateway.entry_point.isGeoGitServer()        
     except Exception, e:        
         _logger.debug("GeoGit gateway not started. Will try to start it")
@@ -38,7 +48,10 @@ def _connect():
                 _proc = subprocess.Popen(os.path.join(geogitPath, "geogit-gateway"), stdout=subprocess.PIPE, stdin=subprocess.PIPE)
     
             time.sleep(3) #improve this and wait until the "server started" string is printed out                            
-            _gateway = JavaGateway()
+            if _geogitPort is None:
+                _gateway = JavaGateway()
+            else:
+                _gateway = JavaGateway(GatewayClient(port = _geogitPort))
             _gateway.entry_point.isGeoGitServer()                   
         except Exception, e:
             _logger.error("Could not start gateway (%s)" % (str(e))) 
@@ -125,10 +138,35 @@ class Py4JCLIConnector(CLIConnector):
         return _runGateway(commands, self.repo.url)
 
     def setRepository(self, repo):
+        '''
+        Sets the repository to use when later passing commands to this connector using the "run" method
+        '''
         self.repo = repo    
         
     def checkIsAlive(self):
         _connect()
+                    
+    def setGeoGitPath(self, path):
+        '''
+        Sets the path to GeoGit, to be used in case the gateway is not started, to try to run it
+        '''
+        setGeoGitPath(path)
+    
+    def setGatewayPort(self, port):
+        '''
+        Sets the port to use for connecting to the gateway.
+        
+        If a connection has already been made using this connector, the existing gateway client 
+        object will be deleted
+        
+        If when creating that connection it was necessary to start the geogit-gateway (that is, if
+        geogit-gateway was not started manually but automatically by this connector), the gateway server
+        will be shutdown
+        
+        If the passed port is equal to the previous port, this method will do nothing, and all the above 
+        does not apply 
+        '''        
+        setGatewayPort(port)
         
         
 
