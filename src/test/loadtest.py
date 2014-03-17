6f5cfdc503a5d7276749559b4a6a2e3267e2c62e
Victor Olaya <volayaf@gmail.com>
2014-03-17 12:48:26 +0100

    added module with load tests

---
+++ src/test/loadtest.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import os
+import time
+from geogitpy.repo import Repository
+import unittest
+
+class GeogitLoadTest(unittest.TestCase):            
+
+    def getTempRepoPath(self):
+        return os.path.join(os.path.dirname(__file__), "temp", str(time.time())).replace('\\', '/')
+
+    def getClonedRepo(self):
+        dst = self.getTempRepoPath()
+        return self.repo.clone(dst)  
+    
+    def testLargeDiff(self):
+        repo = Repository(self.getTempRepoPath(), init=True)
+        path = os.path.join(os.path.dirname(__file__), "data", "shp", "1", "parks.shp")
+        repo.importshp(path)   
+        repo.addandcommit("message")
+        path = os.path.join(os.path.dirname(__file__), "data", "shp", "elevation", "elevation.shp")
+        repo.importshp(path)  
+        repo.addandcommit("message_2")
+        s = repo.diff("HEAD~1", "HEAD")
+        
+    def testLargeHistory(self):
+        NUMPOINTS = 6000
+        repo = Repository(self.getTempRepoPath(), init=True)
+        path = os.path.join(os.path.dirname(__file__), "data", "shp", "elevation", "elevation.shp")
+        repo.importshp(path)
+        repo.add()   
+        for i in xrange(NUMPOINTS):
+            feature = "elevation/" + str(i+1);            
+            message = "message " + str(i+1)
+            repo.commit(message, [feature])
+        log = repo.log()
+        
+        
+        
